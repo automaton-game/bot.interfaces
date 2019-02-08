182dbff178e352693208246860639b7c3f974441
Hernan Lavrencic <hernan.hnet@gmail.com>
2019-02-07 21:32:33 -0300

    timer de refresco cada 3 seg para el jugador inactivo

--- AutomataNETjuegos.Web/ClientApp/src/app/juegoManual/juegoManual.component.ts
+++ AutomataNETjuegos.Web/ClientApp/src/app/juegoManual/juegoManual.component.ts
@@ -2,12 +2,15 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { JuegoManualResponse } from './modelos/juegoManualResponse';
 import { AccionRobot } from './modelos/accionRobot';
+import { timer } from 'rxjs/observable/timer';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-juegoManual-component',
   templateUrl: './juegoManual.component.html',
 })
 export class JuegoManualComponent implements OnInit {
+  private suscripcionRefresco: Subscription;
 
   public juegoManualResponse: JuegoManualResponse;
 
@@ -32,7 +35,10 @@ export class JuegoManualComponent implements OnInit {
     this.http.get<JuegoManualResponse>(this.baseUrl + 'api/Tablero/ObtenerTablero?idTablero=' + this.idTablero)
       .subscribe(result => {
         this.juegoManualResponse = result;
-        this.idJugador = result.jugadores[0];
+        if (!this.idJugador) {
+          this.idJugador = result.jugadores[0];
+          this.verificarTurnoJugadorActual();
+        }
       }, (err: HttpErrorResponse) => this.errores = err.error.errors.map(m => m.message));
   }
   
@@ -40,7 +46,29 @@ export class JuegoManualComponent implements OnInit {
     this.http.post<JuegoManualResponse>(this.baseUrl + 'api/Tablero/AccionarTablero', { idTablero: this.idTablero, idJugador: this.idJugador, accionRobot: accionRobot  })
       .subscribe(result => {
         this.juegoManualResponse = result;
+        this.verificarTurnoJugadorActual();
       }, (err: HttpErrorResponse) => this.errores = err.error.errors.map(m => m.message));
+  }
+
+ 
+
+  public verificarTurnoJugadorActual() {
+    const miTurno = this.juegoManualResponse.jugadorTurnoActual == this.idJugador;
+    this.frenarRefresco();
+    if (!miTurno) {
+      this.suscripcionRefresco = this.iniciarTimerRefresco();
+    }
+  }
+
+  private frenarRefresco() {
+    if (this.suscripcionRefresco) {
+      this.suscripcionRefresco.unsubscribe();
+    }
+  }
+
+  private iniciarTimerRefresco() {
+    const suscripcionRefresco = timer(3000, 3000).subscribe(count => { this.obtenerTablero(); });
+    return suscripcionRefresco;
   }
 
   ngOnInit(): void {
